6527ea0e6a760025d4da26ded0d0c873a3950b88
HelloRusk <36184621+7ma7X@users.noreply.github.com>
2019-01-23 12:22:02 +0900

    logging: true 削除

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,7 +3,6 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
   {
-    logging: true,
     operatorsAliases: false
   });
 
