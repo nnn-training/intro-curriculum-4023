393810d1c660b54809b6d002293dc27bebcf8d19
Satoshi Nakagawa <nakasen20th@gmail.com>
2021-09-13 21:08:50 +0900

    Herokuの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ User.sync().then(() => {
 
 
 var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2f831cb3d4aac02393aa';
-var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '89e30ee6428497aadc14';
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '983b21998f643a9a21309f914c2ab3fedbdeaabe';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
