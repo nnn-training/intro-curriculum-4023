7765a751e67e6eab711322791bcd8df74b3d3bc7
yuhsug <whitetime47@gmail.com>
2022-03-16 16:55:21 +0900

    promise-chain -> async/await

--- routes/comments.js
+++ routes/comments.js
@@ -4,18 +4,21 @@ const router = express.Router();
 const authenticationEnsurer = require('./authentication-ensurer');
 const Comment = require('../models/comment');
 
-router.post('/:scheduleId/users/:userId/comments', authenticationEnsurer, (req, res, next) => {
-  const scheduleId = req.params.scheduleId;
-  const userId = req.params.userId;
-  const comment = req.body.comment;
+router.post(
+  '/:scheduleId/users/:userId/comments',
+  authenticationEnsurer,
+  async (req, res, next) => {
+    const scheduleId = req.params.scheduleId;
+    const userId = req.params.userId;
+    const comment = req.body.comment;
 
-  Comment.upsert({
-    scheduleId: scheduleId,
-    userId: userId,
-    comment: comment.slice(0, 255)
-  }).then(() => {
+    await Comment.upsert({
+      scheduleId: scheduleId,
+      userId: userId,
+      comment: comment.slice(0, 255)
+    });
     res.json({ status: 'OK', comment: comment });
-  });
-});
+  }
+);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -10,23 +10,19 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const title = '予定調整くん';
   if (req.user) {
-    Schedule.findAll({
+    const schedules = await Schedule.findAll({
       where: {
         createdBy: req.user.id
       },
       order: [['updatedAt', 'DESC']]
-    }).then((schedules) => {
-      schedules.forEach((schedule) => {
-        schedule.formattedUpdatedAt = dayjs(schedule.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
-      });
-      res.render('index', {
-        title: title,
-        user: req.user,
-        schedules: schedules
-      });
+    })
+    res.render('index', {
+      title: title,
+      user: req.user,
+      schedules: schedules
     });
   } else {
     res.render('index', { title: title, user: req.user });
