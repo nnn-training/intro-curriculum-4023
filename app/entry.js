46ebbdcc7908deec110ef67a16746042572cf152
CheeseGue <otsukads@gmail.com>
2023-05-28 22:22:34 +0900

    楽しい時間をありがとう

--- app/entry.js
+++ app/entry.js
@@ -40,3 +40,16 @@ buttonSelfComment.on('click', () => {
       });
   }
 });
+
+const shareUrl = window.location.href;
+const shareUrlInput = $('#share-url');
+const shareUrlCopyButton = $('#copy-button');
+
+shareUrlInput.val(shareUrl);
+
+shareUrlCopyButton.on('click', () => {
+  navigator.clipboard.writeText(shareUrl).then(() => {
+    shareUrlCopyButton.text('Copied!');
+    setTimeout(() => shareUrlCopyButton.text('Copy'), 1000);
+  });
+});
