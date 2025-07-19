adaa5bdd5c3af2f9b68c07eb51dc31b5611b655a
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-07-19 11:54:35 +0900

    improve: middlewareをcreateMiddlewareを使って作成するように

--- src/middlewares/ensure-authenticated.js
+++ src/middlewares/ensure-authenticated.js
@@ -1,12 +1,14 @@
+const { createMiddleware } = require('hono/factory');
+
 // ページの閲覧に認証を必須にするミドルウェア
 function ensureAuthenticated() {
-  return async (c, next) => {
-    const session = c.get("session");
+  return createMiddleware(async (c, next) => {
+    const session = c.get('session');
     if (!session.user) {
-      return c.redirect("/login?from=" + c.req.path);
+      return c.redirect('/login?from=' + c.req.path);
     }
     await next();
-  };
+  });
 }
 
 module.exports = ensureAuthenticated;
