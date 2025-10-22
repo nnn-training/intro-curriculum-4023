d8007030a56d57057f7345076bd1c23d488e94bc
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-10-22 14:05:00 +0900

    hotfix: オープンリダイレクタ脆弱性対策のための正規表現によるリダイレクト条件の強化

--- src/app.js
+++ src/app.js
@@ -83,7 +83,7 @@ app.get("/auth/github", async (c) => {
 
   const loginFrom = getCookie(c, "loginFrom");
   // オープンリダイレクタ脆弱性対策
-  if (loginFrom && loginFrom.startsWith('/')) {
+  if (loginFrom && /^\/(?!\/)[\w\-./?=&%+#:]*$/.test(loginFrom)) {
     deleteCookie(c, "loginFrom");
     return c.redirect(loginFrom);
   } else {
